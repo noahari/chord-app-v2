530ea8cc2a3cfac4529a675d4afd69cb1e17dbde
Flavor Yurtle <xxxwaifu4laifuxxx@gmail.com>
2017-12-10 12:40:14 -0500

    added updateTable() method

--- src/main/java/frontend/NotationPanel.java
+++ src/main/java/frontend/NotationPanel.java
@@ -11,16 +11,16 @@ import javax.swing.*;
 public class NotationPanel extends Panel {
     private UI userInterface;
     private String title;
-    private ArrayList<DrawnChord> drawnChordList;
+    //private ArrayList<DrawnChord> drawnChordList;
     private JPanel panel1;
     private JTable table1;
-    private final String[] COL_NAMES = { "Root", "Extension", "Duration" };
+    private final String[] COL_NAMES = new String[]{"Root","Extension","Duration"};
 
     //JTable table1 = new JTable(ChordChart.chordList);
     public void draw(){
         // uhhh
     }
-
+/*
     // if this sucks just delete it:
     public void updateDrawnChordList(){
         ArrayList<DrawnChord> retlist = new ArrayList<>();
@@ -28,5 +28,12 @@ public class NotationPanel extends Panel {
             retlist.add(new DrawnChord((Chordy) chord));
         }
         this.drawnChordList = retlist;
+    }
+*/
+    public void updateTable(){
+	    this.table1 = new JTable(
+			    userInterface.chordChart.getChordList().toTableArray(),
+			    COL_NAMES
+			    );
     }
 }
