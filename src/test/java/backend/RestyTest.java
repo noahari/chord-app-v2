04fde6a9fc786c85021762c9c8889eb6d5a35843
noahari <noahari@gmail.com>
2017-12-11 21:09:31 -0500

    Resty Test coverage @100%

---
+++ src/test/java/backend/RestyTest.java
@@ -0,0 +1,26 @@
+package backend;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RestyTest {
+    @Test
+    public void isRest() throws Exception {
+        Resty restytesty = new Resty(Duration.WHOLE);
+        assertEquals(true, restytesty.isRest());
+    }
+
+    @Test
+    public void getRow() throws Exception {
+        Resty restytesty = new Resty(Duration.WHOLE);
+        String[] testArray = new String[]{
+                "R",
+                "",
+                "WHOLE"
+                //This test fails **IT SHOULD RETURN "WHOLE", instead it returns 1.0**
+                // since a rest is a note, and therefore the duration is represented numerically,
+                //therefore, we need to update this method for the sake of UI display consistency
+        };
+        assertEquals(testArray, restytesty.getRow());
+    }
+}
