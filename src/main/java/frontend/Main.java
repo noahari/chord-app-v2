9e55f5d961af82711c884f2f1683316135eb4b81
Andrew George <ageorge@vassar.edu>
2017-12-12 22:46:41 -0500

    Setup Main to ask for .chordface file and then creates a chart from file

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -3,11 +3,31 @@ package frontend;
 import backend.ChordChart;
 
 import javax.swing.*;
+import javax.swing.filechooser.*;
 
 public class Main {
-    final JFileChooser fc = new JFileChooser();
+    static JFileChooser fc = new JFileChooser();
+    static UIBuilder uiBuilder = new GenericUIBuilder();;
 
     public static void main(String[] args) {
+        // ask if open an old file or create a new one
 
+        // if opening old file
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView());
+        jfc.setDialogTitle("Select a chart");
+        jfc.setAcceptAllFileFilterUsed(false);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("ChordFace Files", "chordface");
+        jfc.addChoosableFileFilter(filter);
+
+        int returnValue = jfc.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            String path = jfc.getSelectedFile().getPath();
+            System.out.println(path);
+            ChordChart chart = ChordChart.fileToChart(path);
+            System.out.println(chart.toString());
+            chart.play();
+
+            uiBuilder.makeUI(chart, new KeyMore("Cmaj"));
+        }
     }
 }
