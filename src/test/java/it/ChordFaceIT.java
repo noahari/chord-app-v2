ac90a4c0c0f8b5d55c4b0a70e5578d3f7410ede9
Colby Morrison <colbyacts@gmail.com>
2017-12-14 21:15:01 -0500

    Integration test

--- src/test/java/it/ChordFaceIT.java
+++ src/test/java/it/ChordFaceIT.java
@@ -1,6 +1,8 @@
 package it;
 
 import backend.ChordChart;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import frontend.*;
@@ -8,9 +10,20 @@ import frontend.*;
 
 public class ChordFaceIT {
 
+    private UIBuilder builder;
+
+    @Before
+    public void setUp(){
+        builder = new GenericUIBuilder();
+    }
+
+    @After
+    public void tearDown(){
+        builder = null;
+    }
+
     @Test
     public void chordFaceIt(){
-        UIBuilder builder = new GenericUIBuilder();
         ButtonsPanel buttonsPanel = builder.getButtonsPanel();
         buttonsPanel.getFirstButton().doClick();
         ChordChart chart = builder.getUI().getChordChart();
@@ -19,7 +32,10 @@ public class ChordFaceIT {
 
     @Test
     public void chordFaceIt2(){
-
+        GlobalParametersPanel gpp = builder.getGlobalParamsPanel();
+        gpp.getKeys().setSelectedIndex(2);
+        ChordButton button = builder.getButtonsPanel().getFirstButton();
+        assertEquals("D", button.getText());
     }
 
 }
